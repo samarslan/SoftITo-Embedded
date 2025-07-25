b51b7e98b28df89217af7995a584dfb11f10e6fd
Samet <sam.arslan@outlook.com>
2025-07-25 19:59:20 +0300

    Saat, gün çevirimi eklendi

--- T25/Operatorler.c
+++ T25/Operatorler.c
@@ -20,9 +20,17 @@ int f3() {
 
 
 int main() {
-    int ival= 0b10110110101;
+  //  int ival= 0b10110110101;
 
-    int result = f1()+f2()*5;
+ //   int result = f1()+f2()*5;
 
-    printf("%d",result);
+    int day, hour, min, sec;
+    scanf("%d%d%d%d", &day, &hour, &min, &sec);
+    min = min + sec/60;
+    sec = sec % 60;
+    hour = hour + min/60;
+    min = min % 60;
+    day = day + hour/24;
+    hour = hour % 24;
+    printf("%d\n%d\n%d\n%d", day, hour, min, sec);
 }
