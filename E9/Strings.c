04f1b559bbebbe2b4aa5250fcb8c89f5aef6d915
sametarslan <sam.arslan@outlook.com>
2025-09-11 09:44:48 +0300

    E9 edit

--- E9/Strings.c
+++ E9/Strings.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define BUFFER_MAX_SIZE 2000
+
 uint32_t strlen(char* buffer) {
     uint32_t i=0;
     while (*(buffer+i)!='\0') {
@@ -17,29 +19,19 @@ void strcat(char* buffer1, char* buffer2) {
     uint32_t length1 = strlen(buffer1);
     uint32_t length2 = strlen(buffer2);
     char *c = buffer1+length1;
-
-    for (uint32_t i=0;i<length2;i++) {
-        *(c+i) = buffer2[i];
+    uint32_t i=0;
+    *(c+i++) = ' ';
+    for (;i<length2+1;i++) {
+        *(c+i) = buffer2[i-1];
     }
+    *(c+i+1) = '\0';
 }
 
 int main() {
-    char buffer[] = "samet";
-    char *pname = buffer;
-    uint32_t length = strlen(buffer);
+    char buffer[BUFFER_MAX_SIZE] = "Samet";
+    strcat(buffer, "Arslan");
 
+    puts(buffer);
 
-    // pname=pname+length;
-    // *pname = 'x';
-    // pname++;
-    // *pname = 'y';
-    // pname++;
-    // *pname = '\0';
-
-
-    strcat(buffer, "mahmut");
-    for (uint32_t j=0;j<11;j++) {
-        printf("%c",buffer[j]);
-    }
     return 0;
 }
